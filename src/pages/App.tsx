ddea0a1660c4ce32ad90cd46ce9531363e33f81d
Emerson Gonçalves dos Santos <emer_gs@hotmail.com>
2023-04-13 23:27:55 +0000

    feat:inclusão das imagens e logos no assets

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from "../assets/images/logo-primary.svg"
 
 function App() {
   return (
     <div className="App">
       initial
+      <img src={logo} alt="" />
     </div>
   );
 }
