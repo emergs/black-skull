438e8727eff829069c05c39acad8bb181ceb719c
emerson gonçalves <emer_gs@hotmail.com>
2023-04-16 00:05:29 -0300

    feat:efeitos de hover no componente ProductsCategory

--- src/components/ProductsCategory/styled.ts
+++ src/components/ProductsCategory/styled.ts
@@ -10,7 +10,7 @@ export const ProductsCategoryStyled = styled.section`
     padding:56px;
     padding-bottom: 64px;
 
-    li{
+    li, figure{
       width: 178px;
       height: 210px;
 
@@ -19,12 +19,41 @@ export const ProductsCategoryStyled = styled.section`
         height: 178px;
         background: var(--background);
         border-radius: 50%;
-        padding: 35px;
-      }
+        //padding: 35px;
+        position: relative;
+        z-index: 0;
 
-      img{
-        width: 108px;
-        height: 108px;
+        ::before{
+          content: '';
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%,-50%);
+          width: 0%;
+          height: 0%;
+          transition: 0.5s ease;
+          background-color: var(--orange1);
+          outline: 5px solid var(--orange2);
+          z-index: -1;
+        }
+
+        :hover::before{
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+
+        img{
+          width: 100%;
+          height: 100%;
+          z-index: 1;
+          padding: 35px;
+
+          :hover{
+            transform: scale(1.5);
+            transition: 0.5s ease;
+          }
+        }
       }
 
       figcaption{
